de84928d02cf603532ebbd3464fa06a5fa516c7c
Vishota <vishotax3@gmail.com>
2024-09-23 23:59:42 +0300

    ready

---
+++ app/src/main.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+
+const app = express()
+const port = 8000
+
+app.all('/*', (req, res) => {
+    const data = {
+        method: req.method,
+        path: req.path,
+        protocol: req.protocol,
+        host: req.get('host'),
+        ip: req.ip,
+        headers: req.headers,
+        query: req.query,
+        body: req.body,
+        cookies: req.cookies,
+        params: req.params
+    }
+    res.send(data)
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
